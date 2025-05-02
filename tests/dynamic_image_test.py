a6d8e11f29fdd5829c62b260a96a654aeb3d46fe
Ted <58144503+BUSH222@users.noreply.github.com>
2025-05-03 01:27:40 +0300

    let's see where it crashes

--- tests/dynamic_image_test.py
+++ tests/dynamic_image_test.py
@@ -1,6 +1,5 @@
 import dearpygui.dearpygui as dpg
 from PIL import Image
-import time
 
 
 image_path = "assets/camera_off.png"
@@ -9,17 +8,22 @@ width, height = image.size
 image_data = list(image.getdata())
 image_data = [c / 255.0 for pixel in image_data for c in pixel]
 
-
+print('entry to tex reg')
 with dpg.texture_registry():
     texture_id = dpg.add_static_texture(width, height, image_data)
-
+print('prepping window')
 
 with dpg.window(label="dsafa"):
     dpg.add_image(texture_id)
+print('prepping create viewport')
 
 dpg.create_viewport(title="nkfdjsabdh", width=width+40, height=height+60)
+print('a')
 dpg.setup_dearpygui()
+print('b')
 dpg.show_viewport()
+print('c')
 dpg.start_dearpygui()
-time.sleep(10)
+print('d')
 dpg.destroy_context()
+print('e')
