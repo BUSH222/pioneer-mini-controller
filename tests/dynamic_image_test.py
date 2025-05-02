246f1c6c00303b8b0a08ce79529fdaba3b90422d
Ted <58144503+BUSH222@users.noreply.github.com>
2025-05-03 01:21:31 +0300

    tests

---
+++ tests/dynamic_image_test.py
@@ -0,0 +1,23 @@
+import dearpygui.dearpygui as dpg
+from PIL import Image
+
+
+image_path = "assets/camera_off.png"
+image = Image.open(image_path).convert("RGBA")
+width, height = image.size
+image_data = list(image.getdata())
+image_data = [c / 255.0 for pixel in image_data for c in pixel]
+
+
+with dpg.texture_registry():
+    texture_id = dpg.add_static_texture(width, height, image_data)
+
+
+with dpg.window(label="dsafa"):
+    dpg.add_image(texture_id)
+
+dpg.create_viewport(title="nkfdjsabdh", width=width+40, height=height+60)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
